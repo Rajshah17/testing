ef8496648a46bcfecfa98b52314b82b39de877a2
Rajshah17 <123472964+Rajshah17@users.noreply.github.com>
2023-04-23 12:16:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Register from './student_registration_component/Register';
+
+function App() {
+
+  return (
+    <>
+      <Register/>
+         </>
+  )
+}
+export default App
